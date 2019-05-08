c968382aa0eeaf03da4f02715ea9274ea49fa3eb
Shawn Chin <shawn@qwil.io>
2019-05-08 10:09:10 +0100

    QWIL-1486: Make pip installable as v1.0.0

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+packages = setuptools.find_packages(include=["gasofo*"])
+
+
+setuptools.setup(
+    name="gasofo",
+    version="1.0.0",
+    author="Shawn Chin",
+    author_email="shawn@qwil.io",
+    description="Qwil's hexagonal architecture framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.tooling-dev.private.qwil.network/open/gasofo",
+    packages=packages,
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
